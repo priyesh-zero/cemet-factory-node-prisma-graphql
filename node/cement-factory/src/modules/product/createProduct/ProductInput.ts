adf32e76c4a742e3b806c2f872991812216ab7c6
priyesh-zero <priyesh.zero@gmail.com>
2020-08-03 20:04:15 +0530

    💡 Removed imageFile field from the create product input

--- node/cement-factory/src/modules/product/createProduct/ProductInput.ts
+++ node/cement-factory/src/modules/product/createProduct/ProductInput.ts
@@ -1,29 +1,24 @@
-import {InputType, Field} from "type-graphql";
-import {Min,  Length} from "class-validator";
-import {Upload} from "src/types/Upload";
-import {GraphQLUpload} from "graphql-upload";
+import { InputType, Field } from "type-graphql";
+import { Min, Length } from "class-validator";
+import { Upload } from "src/types/Upload";
+import { GraphQLUpload } from "graphql-upload";
 
 @InputType()
 export class ProductInput {
-
     @Field()
     @Length(3, 25)
-    name: string
+    name: string;
 
     @Field()
-    description: string
-
-    @Field()
-    @Min(1)
-    price: number
+    description: string;
 
     @Field()
     @Min(1)
-    quantity: number
+    price: number;
 
-    @Field(() => GraphQLUpload, { nullable: true })
-    imageFile: Upload
+    @Field()
+    @Min(1)
+    quantity: number;
 
-    image: string = "default.jpg"
-
+    image: string = "default.jpg";
 }
